835fba23925bb3bdeae8a98c6b1cbe5ed9ac101a
Keiji, Yoshimi <walf443@gmail.com>
2011-10-29 16:41:49 +0900

    write client.

--- lib/Bloomd/Client.pm
+++ lib/Bloomd/Client.pm
@@ -3,6 +3,68 @@ package Bloomd::Client;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+use IO::Socket::INET;
+
+sub new {
+    my ($class, %args) = @_;
+
+    $args{server} ||= 'localhost:26006';
+    my $sock = IO::Socket::INET->new(
+        PeerAddr => $args{server},
+        Proto => 'tcp'
+    )
+        or die "Can't connect to @{[ $args{server} ]}";
+    $args{sock} = $sock;
+    bless \%args, $class;
+}
+
+sub set {
+    my ($self, $key) = @_;
+    $self->set_multi($key);
+}
+
+sub set_multi {
+    my ($self, @keys) = @_;
+
+    my $key = join "\t", @keys;
+    my $sock = $self->{sock};
+    print $sock "set $key\r\n";
+    my $line = <$sock>;
+    if ( $line eq "OK\r\n" ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+sub test_multi {
+    my ($self, @keys) = @_;
+
+    my $key = join "\t", @keys;
+    my $sock = $self->{sock};
+    print $sock "test $key\r\n";
+    my $result = {};
+    while ( my $line = <$sock> ) {
+        if ( $line eq "OK\r\n" ) {
+            return $result;
+        } else {
+            if ( $line =~ /^TEST (\S+) (\d)\r\n/ ) {
+                $result->{$1} = $2;
+            }
+        }
+    }
+}
+
+sub test {
+    my ($self, $key) = @_;
+    my $result = $self->test_multi($key);
+    return $result->{$key};
+}
+
+sub DESTROY {
+    my ($self, ) = @_;
+    close $self->{sock};
+}
 
 1;
 __END__
@@ -14,6 +76,10 @@ Bloomd::Client -
 =head1 SYNOPSIS
 
   use Bloomd::Client;
+  my $client = Bloomd::Client->new(server => "localhost:26006");
+  $client->set("hoge");
+  $client->test("hoge") #=> 1 # may be.
+  $client->test("bar") #=> 0
 
 =head1 DESCRIPTION
 
